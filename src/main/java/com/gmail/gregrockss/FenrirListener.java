d7876f3f2140bf47d87c67d44c2147df2e1952f1
Gregory Mason <gregrockss@gmail.com>
2016-03-12 03:15:23 -0500

    Fully Updated for 1.9
    Updated the getBlock line and updated the getItemInHand line.

--- src/main/java/com/gmail/gregrockss/FenrirListener.java
+++ src/main/java/com/gmail/gregrockss/FenrirListener.java
@@ -9,7 +9,6 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class FenrirListener implements Listener {
 	
@@ -18,21 +17,21 @@ public class FenrirListener implements Listener {
 		for (Player player : Bukkit.getServer().getOnlinePlayers()){
 			//checks if the player is an Op
 			if (player.isOp()) {
-				if(evt.getPlayer().getInventory().getItemInMainHand().getTypeId() == Material.BLAZE_ROD.getId()){
+				if(evt.getPlayer().getInventory().getItemInMainHand().getType() == Material.BLAZE_ROD){
 				//maximal distance between player and explosion is 20 blocks
 					evt.getPlayer().getWorld().createExplosion(evt.getPlayer().getTargetBlock((Set<Material>)null, 20).getLocation(), 7, true);
         }
-				if(evt.getPlayer().getInventory().getItemInMainHand().getTypeId() == Material.NETHER_STAR.getId()){
+				if(evt.getPlayer().getInventory().getItemInMainHand().getType() == Material.NETHER_STAR){
 						//maximal distance between player and thunder is 50
 					evt.getPlayer().getWorld().strikeLightning(evt.getPlayer().getTargetBlock((Set<Material>)null, 50).getLocation());
         }
 				//checks if the player has a stick in his hand
-				if(evt.getPlayer().getInventory().getItemInMainHand().getTypeId() == Material.STICK.getId()){
+				if(evt.getPlayer().getInventory().getItemInMainHand().getType() == Material.STICK){
 					//spawns a zombie where the player clicked with the stick
 					evt.getPlayer().getWorld().spawnEntity(evt.getPlayer().getTargetBlock((Set<Material>)null, 15).getLocation(), EntityType.ZOMBIE);
 		}
 				//checks if the player has a Ghast Tear in his hand
-				if(evt.getPlayer().getInventory().getItemInMainHand().getTypeId() == Material.GHAST_TEAR.getId()){
+				if(evt.getPlayer().getInventory().getItemInMainHand().getType() == Material.GHAST_TEAR){
 					//spawns a ghast where the player clicked with the Ghast Tear
 					evt.getPlayer().getWorld().spawnEntity(evt.getPlayer().getTargetBlock((Set<Material>)null, 15).getLocation(), EntityType.GHAST);
 				}
--- src/main/java/com/gmail/gregrockss/Main.java
+++ src/main/java/com/gmail/gregrockss/Main.java
@@ -2,7 +2,7 @@ package com.gmail.gregrockss;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Fenrir extends JavaPlugin {
+public class Main extends JavaPlugin {
 	public void onEnable(){
 		getServer().getPluginManager().registerEvents(new FenrirListener(), this);
 	}
